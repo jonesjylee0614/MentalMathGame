a7147f59ea7106d6c2a2f7965ff39dd1138a1213
nlwu <47283923@qq.com>
2025-10-06 22:45:12 +0800

    Set base path for deployment and update BrowserRouter basename to '/mentalmath' for proper routing in production environment.

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import './styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/mentalmath">
       <GameProvider>
         <App />
       </GameProvider>
